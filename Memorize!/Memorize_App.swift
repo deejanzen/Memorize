7a2113299ff6627228c640c069894f7cc8fee7d7
Dustin Janzen <deejanzen@gmail.com>
2024-10-19 12:03:55 -0500

    Initial Commit

---
+++ Memorize!/Memorize_App.swift
@@ -0,0 +1,17 @@
+//
+//  Memorize_App.swift
+//  Memorize!
+//
+//  Created by Dustin Janzen on 10/19/24.
+//
+
+import SwiftUI
+
+@main
+struct Memorize_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Memorize!Tests/Memorize_Tests.swift
@@ -0,0 +1,16 @@
+//
+//  Memorize_Tests.swift
+//  Memorize!Tests
+//
+//  Created by Dustin Janzen on 10/19/24.
+//
+
+import Testing
+
+struct Memorize_Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
